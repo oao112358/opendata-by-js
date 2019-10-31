b8b4b3cfd4f78e56128f726cd5a0368069d4e81b
I27954 <I27954@icsc.com.tw>
2019-10-31 16:09:23 +0800

    second commit

--- opendata-by-js/js/data.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var epaAPI = "http://opendata.epa.gov.tw/ws/Data/UV/?$orderby=PublishAgency&$skip=0&$top=1000&format=json&callback=?";
-  $.getJSON( epaAPI, function(){
-    format: "json"
-  }).done(function(data) {
-    	console.log(data);
-    });
-})();
